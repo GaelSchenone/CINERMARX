007b30dfe13404c75a590d67132d5580d98bbad2
similaralamuerte <119982369+D4rkMan9@users.noreply.github.com>
2025-10-30 20:32:16 -0300

    Srrano nos ama, test
    t

--- M4/src/main/java/CINEMARX/M4/M4.java
+++ M4/src/main/java/CINEMARX/M4/M4.java
@@ -14,6 +14,7 @@ public class M4 {
      * @param args the command line arguments
      */ 
     public static void main(String args[]) {
-        System.out.println("serrano la concha de tu madre");
+        System.out.println("serrano te amo");
+        
     }
 }
